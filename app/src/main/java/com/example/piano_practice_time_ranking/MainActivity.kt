2d3aae07619a742dadf203ddb158a131f4300d25
Yuki Naka <yukinaka2168@au.com>
2024-10-16 13:30:47 +0900

    「MainActivity」に「NavHostFragment」と「BottomNavigationView」と「NavigationUI」を追加。

---
+++ app/src/main/java/com/example/piano_practice_time_ranking/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.piano_practice_time_ranking
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+
+        NavigationUI.setupWithNavController(bottomNavigationView, navController)
+    }
+}

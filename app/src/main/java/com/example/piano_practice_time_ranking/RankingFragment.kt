68c64edd208dca2cdcf13607156d405e2b9281c0
Yuki Naka <yukinaka2168@au.com>
2024-10-16 15:52:45 +0900

    「RankingFragment」内 「onCreateView」内を修正。

--- app/src/main/java/com/example/piano_practice_time_ranking/RankingFragment.kt
+++ app/src/main/java/com/example/piano_practice_time_ranking/RankingFragment.kt
@@ -14,15 +14,12 @@ class RankingFragment : Fragment() {
     private var _binding: FragmentRankingBinding? = null
     private val binding get() = _binding!!
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_ranking, container, false)
+    ): View {
+        _binding = FragmentRankingBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
